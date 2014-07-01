2fa8084879b641f61dba9e896fec10e1f01850a5
ilovearsenal93 <ongsatthu@gmail.com>
2014-07-01 11:33:10 +0700

    Finish update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,46 @@
 class UsersController < ApplicationController
-   def show
-    @user = User.find(params[:id])
-  end
-  def new
-  	@user = User.new
-  end
-  def create
-    @user = User.new(user_params)
-    if @user.save
+	before_action :signed_in_user, only: [:edit, :update]
+	def show
+		@user = User.find(params[:id])
+	end
+	def new
+		@user = User.new
+	end
+	def edit
+		@user = User.find(params[:id])
+	end
+	def create
+		@user = User.new(user_params)
+		if @user.save
       # Handle a successful save.
       sign_in @user
       flash[:success] = "Welcome to the Blog App!"
       redirect_to @user
-    else
-      render 'new'
-    end
+  else
+  	render 'new'
   end
+end
+def update
+	@user = User.find(params[:id])
+	if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+  else
+  	render 'edit'
+  end
+end
 
-  private
+private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+def user_params
+	params.require(:user).permit(:name, :email, :password,
+		:password_confirmation)
+end
+def signed_in_user
+	unless signed_in?
+		flash[:notice] = "Please sign in."
+		redirect_to signin_url
+	end
+end
 end
