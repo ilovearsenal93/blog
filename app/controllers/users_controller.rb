cacf7a73e98fdfbf893708e54b8a97099bce8b10
ilovearsenal93 <ongsatthu@gmail.com>
2014-07-01 13:24:01 +0700

    Finish show all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :signed_in_user, only: [:edit, :update]
+	 before_action :correct_user,   only: [:edit, :update]
 	def show
 		@user = User.find(params[:id])
 	end
@@ -30,7 +31,9 @@ def update
   	render 'edit'
   end
 end
-
+ def index
+    @users = User.paginate(page: params[:page])
+  end
 private
 
 def user_params
@@ -39,8 +42,12 @@ def user_params
 end
 def signed_in_user
 	unless signed_in?
-		flash[:notice] = "Please sign in."
-		redirect_to signin_url
-	end
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
 end
+def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
